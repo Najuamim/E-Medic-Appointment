d2d742955b3110a23bb060232969f08704cfa30d
t3ng221 <androsiam001@gmail.com>
2022-02-06 01:48:00 +0600

    final commit

--- src/usersx/Doctor/DocCard.jsx
+++ src/usersx/Doctor/DocCard.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 const DocCard = (props) => {
-  const notify = () => toast.success("Appointment Cancled ");
+  const notify = () => toast.success("Appointment Canceled ");
   const {
     _id,
     Doctor,
